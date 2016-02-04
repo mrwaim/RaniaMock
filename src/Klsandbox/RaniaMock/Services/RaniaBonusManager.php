1e2246f7a2bc9994e7052464f5512b69d5f0a443
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-02-04 11:01:57 +0800

    init

---
+++ src/Klsandbox/RaniaMock/Services/RaniaBonusManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Klsandbox\RaniaMock\Services;
+
+use App;
+use App\Models\User;
+use App\Models\Bonus;
+use Klsandbox\BonusModel\Models\BonusPayout;
+use Klsandbox\BonusModel\Models\BonusType;
+use Klsandbox\BonusModel\Services\BonusCommand;
+use Klsandbox\BonusModel\Services\BonusManager;
+use Klsandbox\OrderModel\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use DateTime;
+use Log;
+
+class RaniaBonusManager implements BonusManager
+{
+    public function getExpiry(BonusCommand $bonusCommand)
+    {
+        $date = $bonusCommand->order->created_at;
+        return $date->addMonth(1)->endOfMonth();
+    }
+
+    public function resolveBonus(Order $order)
+    {
+        $first = new DateTime();
+
+        assert($order->isApproved(), 'order is not accepted');
+
+        if ($order->user->upLevel->role->name != 'admin') {
+            self::payIntroducerBonus($order, $order->user->upLevel);
+        }
+        self::payIntroducerBonus($order, $order->user);
+    }
+
+    public function resolveBonusCommandsForOrderUserDetails($order_id, Carbon $created_at, Order $order, $user)
+    {
+        return [];
+    }
+
+    // Payment methods
+    private static function payIntroducerBonus(Order $order, User $user)
+    {
+        assert($order->isApproved(), 'order is not accepted');
+
+        //echo "FIRST ORDER => GIVE INTRODUCER BONUS\n";
+        //echo "ORDER_USER " . $order->user . PHP_EOL;
+        //echo "INTRODUCER " . $user . PHP_EOL;
+
+        $bonus = Bonus::create([
+            'created_at' => $order->approved_at,
+            'updated_at' => $order->updated_at,
+            'workflow_status' => 'ProcessedByReceiver',
+            // Null - let them choose
+            'bonus_payout_id' => null,
+            'bonus_type_id' => BonusType::IntroducerBonus()->id,
+            'awarded_by_user_id' => 2,
+            'awarded_to_user_id' => $user->id,
+            'order_id' => $order->id,
+        ]);
+
+        //echo "BONUS_PAID " . $bonus . PHP_EOL;
+    }
+}
---
+++ src/Klsandbox/RaniaMock/Services/RaniaOrderManager.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Klsandbox\RaniaMock\Services;
+
+use App;
+use App\Models\NotificationRequest;
+use Klsandbox\BonusModel\Services\BonusManager;
+use Klsandbox\OrderModel\Models\Order;
+use Klsandbox\OrderModel\Models\OrderStatus;
+use App\Models\User;
+use Carbon\Carbon;
+use Auth;
+use Klsandbox\OrderModel\Services\OrderManager;
+use Klsandbox\SiteModel\Site;
+use Log;
+
+class RaniaOrderManager implements OrderManager
+{
+    protected $bonusManager;
+
+    public function __construct(BonusManager $bonusManager)
+    {
+        $this->bonusManager = $bonusManager;
+    }
+
+    public function approveOrder(Order $order, $approved_at = null)
+    {
+        assert(
+            $order->order_status_id == OrderStatus::NewOrderStatus()->id || $order->order_status_id == OrderStatus::FirstOrder()->id || $order->order_status_id == OrderStatus::PaymentUploaded()->id, "Invalid Order to approve $order->id - Status {$order->orderStatus->name}");
+
+        Site::protect($order, "Order");
+
+        if ($order->order_status_id == OrderStatus::FirstOrder()->id) {
+            Site::protect($order->user, "User");
+            $order->user->account_status = 'Approved';
+            $order->user->save();
+        }
+
+        $order->order_status_id = OrderStatus::Approved()->id;
+
+        if (!$approved_at) {
+            $approved_at = new Carbon();
+        }
+
+        $order->approved_at = $approved_at;
+        $order->save();
+
+        $approveId = (Auth::user() ? Auth::user()->id : 0);
+        Log::info("Order approved:$order->id by:" . $approveId);
+
+        User::createUserEvent($order->user, ['created_at' => $approved_at, 'controller' => 'timeline', 'route' => '/order-approved', 'target_id' => $order->id, 'parameter_id' => $approveId]);
+        NotificationRequest::create(['target_id' => $order->id, 'route' => 'order-approved', 'channel' => 'Sms', 'to_user_id' => $order->user->id]);
+
+        $this->bonusManager->resolveBonus($order);
+    }
+
+    public function rejectOrder(Order $order)
+    {
+        assert(
+            $order->order_status_id == OrderStatus::NewOrderStatus()->id || $order->order_status_id == OrderStatus::FirstOrder()->id || $order->order_status_id == OrderStatus::PaymentUploaded()->id, "Invalid Order to approve $order->id - Status {$order->orderStatus->name}");
+
+        Site::protect($order, "Order");
+
+        if ($order->order_status_id == OrderStatus::FirstOrder()->id) {
+            Site::protect($order->user, "User");
+            $order->user->account_status = 'Rejected';
+            $order->user->save();
+        }
+        $order->order_status_id = OrderStatus::Rejected()->id;
+        $order->save();
+
+        Log::info("Order rejected:$order->id by:" . Auth::user()->id);
+
+        User::createUserEvent($order->user, ['controller' => 'timeline', 'route' => '/order-rejected', 'target_id' => $order->id, 'parameter_id' => Auth::user()->id]);
+        NotificationRequest::create(['target_id' => $order->id, 'route' => 'order-rejected', 'channel' => 'Sms', 'to_user_id' => $order->user->id]);
+    }
+
+    public function shipOrder(Order $order, $trackingId)
+    {
+        $order->tracking_id = $trackingId;
+        $order->order_status_id = OrderStatus::Shipped()->id;
+        $order->save();
+
+        User::createUserEvent($order->user, ['controller' => 'timeline', 'route' => '/order-shipped', 'target_id' => $order->id, 'parameter_id' => Auth::user()->id]);
+        NotificationRequest::create(['target_id' => $order->id, 'route' => 'order-shipped', 'channel' => 'Sms', 'to_user_id' => $order->user->id]);
+    }
+
+    public function orderCreated(Order $order) {
+        Log::info("created\t#order:$order->id user:{$order->user->id} status:{$order->orderStatus->name}");
+
+        NotificationRequest::create(['target_id' => $order->id, 'route' => 'new-order', 'channel' => 'Sms', 'to_user_id' => User::admin()->id]);
+
+        NotificationRequest::create(['target_id' => $order->id, 'route' => 'new-downlevel-order', 'channel' => 'Sms', 'to_user_id' => $order->user->referral_id]);
+
+        User::createUserEvent($order->user, ['created_at' => $order->created_at, 'controller' => 'timeline', 'route' => '/new-order', 'target_id' => $order->id]);
+    }
+}
