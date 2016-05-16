92c541ae856673df243b9b78f8e14d5911a51bca
Achmad Fatoni <achmadfatony@gmail.com>
2016-05-16 10:22:26 +0800

    assign user to dropship group if product is membership product

--- src/Klsandbox/RaniaMock/Services/RaniaOrderManager.php
+++ src/Klsandbox/RaniaMock/Services/RaniaOrderManager.php
@@ -2,9 +2,14 @@
 
 namespace Klsandbox\RaniaMock\Services;
 
+use App\Models\Group;
+use App\Models\User;
 use App\Services\UserManager;
 use Klsandbox\BonusModel\Services\BonusManager;
 use Klsandbox\OrderModel\Models\Order;
+use App\Services\MembershipManager\MembershipManagerInterface as MembershipManager;
+use Klsandbox\OrderModel\Models\OrderItem;
+use Klsandbox\OrderModel\Models\Product;
 
 class RaniaOrderManager extends  RaniaOrderManagerWithNoBonus
 {
@@ -12,11 +17,16 @@ class RaniaOrderManager extends  RaniaOrderManagerWithNoBonus
      * @var BonusManager $bonusManager
      */
     protected $bonusManager;
+    /**
+     * @var MembershipManager
+     */
+    private $membershipManager;
 
-    public function __construct(BonusManager $bonusManager, UserManager $userManager)
+    public function __construct(BonusManager $bonusManager, UserManager $userManager, MembershipManager $membershipManager)
     {
         $this->bonusManager = $bonusManager;
         parent::__construct($userManager);
+        $this->membershipManager = $membershipManager;
     }
 
     public function approveOrder(Order $order, $approved_at = null)
@@ -25,6 +35,7 @@ class RaniaOrderManager extends  RaniaOrderManagerWithNoBonus
 
         foreach ($order->orderItems as $orderItem)
         {
+            $this->assignUserToDropshipGroup($orderItem);
             if ($orderItem->productPricing->product->bonusCategory)
             {
                 $this->bonusManager->resolveBonus($orderItem);
@@ -32,5 +43,17 @@ class RaniaOrderManager extends  RaniaOrderManagerWithNoBonus
         }
 
         return $return;
+    }
+
+    public function assignUserToDropshipGroup(OrderItem $orderItem)
+    {
+        $dropShipMembershipProduct = Product::getDropshipMembership();
+        $dropshipGroup = Group::getDropshipGroup();
+        $order = $orderItem->order;
+        $user = User::find($order->user_id);
+
+        if($orderItem->productPricing->product->id = $dropShipMembershipProduct->id){
+            $this->membershipManager->assignGroup($dropshipGroup, $user);
+        }
     }
 }
